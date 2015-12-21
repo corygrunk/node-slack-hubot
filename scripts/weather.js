fda8615e209618804c08d6983adbd3e94aa052f5
Cory Grunkemeyer <cory.grunkemeyer@insidetrack.com>
2015-12-21 16:55:13 -0700

    Changing send to reply

--- scripts/weather.js
+++ scripts/weather.js
@@ -18,14 +18,14 @@ module.exports = function(robot) {
     var APIKEY;
     APIKEY = process.env.HUBOT_OWM_APIKEY || null;
     if (APIKEY === null) {
-      msg.send("HUBOT_OWM_APIKEY environment variable is not provided for hubot-weather");
+      msg.reply("HUBOT_OWM_APIKEY environment variable is not provided for hubot-weather");
     } else {
       msg.http("http://api.openweathermap.org/data/2.5/weather?q=" + msg.match[1] + "&units=metric&APPID=" + APIKEY).header('Accept', 'application/json').get()(function(err, res, body) {
         var data;
         data = JSON.parse(body);
         if (data.cod == '200') {
           var currentTemp = Math.round(data.main.temp * 1.8 + 32) + "°F";
-          msg.send("It is currently " + currentTemp + " in " + data.name + ".");
+          msg.reply("It is currently " + currentTemp + " in " + data.name + ".");
         }
       });
     }
