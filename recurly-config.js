7fd9e4c5f0a679af0479e38b8ed9640f1f4617cd
Cory Grunkemeyer <cory.grunkemeyer@insidetrack.com>
2015-12-23 11:00:15 -0700

    Working bot without Recurly

--- recurly-config.js
+++
@@ -1,8 +0,0 @@
-// recurlyConfig.js
-
-module.exports = {
-  API_KEY: process.env.HUBOT_RECURLY_PRIVATE_API,
-  SUBDOMAIN: process.env.HUBOT_RECURLY_SUBDOMAIN,
-  ENVIRONMENT: 'sandbox',
-  DEBUG: false
-}
